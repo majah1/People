39993b749551aa4f4f89a858f59c222e1dc47285
unknown <hadziahmetovic.maja@gmail.com>
2018-01-07 15:40:28 -0800

    people: create/add/change

---
+++ ConsoleApp1/NewPerson.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persons
+{
+    public class NewPerson
+    {
+        private string id;
+        private int age;
+        private string firstName;
+        private string lastName;
+        private string fullName;
+        private Guid guid;
+
+
+        public int Age { get => age; set => age = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FullName { get => fullName; set => fullName = value; }
+        public string Id { get => id; set => id = value; }
+
+        public NewPerson(int Age, string FirstName, string LastName, string personID)
+
+        {
+            this.Age = Age;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.FullName = FirstName + " " + LastName;
+            this.id = personID;
+        }
+
+        //private void setID()
+        //{
+        //    Guid Id;
+        //   Id= Guid.NewGuid();
+
+        //}
+
+        // private void setID() => this.Id = this.Age + this.firstName;
+        public void PromenaPrezimena(String newLastName) {
+            this.lastName = newLastName;
+            this.FullName = firstName + " " + newLastName;
+        }
+
+        public void PromenaGodina(int noveGodine)
+        {
+            this.Age += noveGodine;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persons
+
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            List<NewPerson> ljudi = new List<NewPerson>();
+            while (true)
+            {
+                Console.WriteLine("Uneti komandu");
+                String command = Console.ReadLine();
+
+                if (command == "nova")
+                {
+                    Console.WriteLine("Uneti ime, prezime i godine sa zarezom");
+
+                    string[] vrednosti = Console.ReadLine().Split(',');
+                    NewPerson newPerson = new NewPerson(int.Parse(vrednosti[2]), vrednosti[0], vrednosti[1], Guid.NewGuid().ToString());
+                    Console.WriteLine("Uneta osoba:" + newPerson.FullName + "," + newPerson.Age + ";" + newPerson.Id);
+                    ljudi.Add(newPerson);
+                }
+
+                else if (command == "izlistaj")
+                {
+                    for (int i = 0; i < ljudi.Count; i++)
+                    {
+                        Console.WriteLine("Full name: " + ljudi[i].FullName + " Age: " + ljudi[i].Age + "  ID:" + ljudi[i].Id);
+                    }
+
+                }
+
+                else if (command == "prezime")
+                {
+                    Console.WriteLine("izabrati osobu po id-ju");
+                    string izabranId = Console.ReadLine();
+
+                    Console.WriteLine("izabrati novo prezime");
+                    string novoPrezime = Console.ReadLine();
+
+                    for (int i = 0; i < ljudi.Count; i++)
+                    {
+                        if (ljudi[i].Id.Equals(izabranId))
+                        {
+                            ljudi[i].PromenaPrezimena(novoPrezime);
+
+                        }
+
+                    }
+
+                }
+
+                else if (command == "ostari")
+                {
+                    Console.WriteLine("izabrati osobu po id-ju");
+                    string izabranId = Console.ReadLine();
+                    for (int i = 0; i < ljudi.Count; i++) {
+                        if (ljudi[i].Id.Equals(izabranId))
+                        Console.WriteLine("Izabrati koliko godina ce ostariti " + ljudi[i].FullName); }
+                    int brojGodina = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < ljudi.Count; i++)
+                    {
+                        if (ljudi[i].Id.Equals(izabranId))
+                        {
+                            ljudi[i].PromenaGodina(brojGodina);
+
+                        }
+
+                    }
+
+                }
+
+
+            }
+        }
+       
+    }
+}
